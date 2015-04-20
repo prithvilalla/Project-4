88fe82258afc075e4f332161489e322b8283d959
prithvilalla <prithvilalla@gmail.com>
2015-04-19 20:09:53 -0400

    Update worm.cc

--- worm.cc
+++ worm.cc
@@ -1,6 +1,7 @@
 /* -*- Mode:C++; c-file-style:"gnu"; indent-tabs-mode:nil; -*- */
 
 #include <iostream>
+#include <time.h>
 
 #include "ns3/core-module.h"
 #include "ns3/network-module.h"
@@ -16,19 +17,17 @@ uint32_t Worm::lastIP = 0;
 TypeId Worm::m_typeId = UdpSocketFactory::GetTypeId ();
 vector<string> Worm::m_listInfected;
 vector<double> Worm::m_time;
+uint32_t Worm::m_connections = 1;
+uint32_t Worm::m_packetSize = 256;
+uint32_t Worm::m_nPackets = 0;
+DataRate Worm::m_dataRate = DataRate ("0.5Mbps");
 
 Worm::Worm ():
   m_node (NULL), 
   m_address (Ipv4Address ()),
   m_port (0),
-  m_txSocket (NULL),
   m_rxSocket (NULL),
-  m_destinationAddress (Address ()),
   m_sinkAddress (Address ()),
-  m_sourceAddress (Address ()),
-  m_packetSize (0), 
-  m_nPackets (0), 
-  m_dataRate (0), 
   m_sendEvent (), 
   m_running (false), 
   m_packetsSent (0),
@@ -41,24 +40,26 @@ Worm::Worm ():
 
 Worm::~Worm ()
 {
-  m_txSocket = NULL;
+  for (uint32_t i = 0; i < m_connections; i++)
+    m_txSocket[i] = NULL;
   m_rxSocket = NULL;
 }
 
 void
-Worm::Setup (Ptr<Node> node, Ipv4Address address, uint32_t port, uint32_t packetSize, uint32_t nPackets, DataRate dataRate, TypeId typeId, bool infected, bool vulnerable, string ID)
+Worm::Setup (Ptr<Node> node, Ipv4Address address, uint32_t port, bool infected, bool vulnerable, string ID)
 {
   m_node = node;
   m_address = address;
   m_port = port;
-  m_typeId = typeId;
-  m_txSocket = Socket::CreateSocket (node, m_typeId);
+  
+  for (uint32_t i = 0; i < m_connections; i++)
+  {
+    m_txSocket.push_back (Socket::CreateSocket (node, m_typeId));
+    m_sourceAddress.push_back (InetSocketAddress (address, port-1-i));
+  }
+  
   m_rxSocket = Socket::CreateSocket (node, m_typeId);
   m_sinkAddress = InetSocketAddress (address, port);
-  m_sourceAddress = InetSocketAddress (address, port-1);
-  m_packetSize = packetSize;
-  m_nPackets = nPackets;
-  m_dataRate = dataRate;
   m_infected = infected;
   m_vulnerable = vulnerable;
   m_ID = ID;
@@ -67,13 +68,17 @@ Worm::Setup (Ptr<Node> node, Ipv4Address address, uint32_t port, uint32_t packet
 void
 Worm::StartApplication (void)
 {
+  srand (time (NULL));
+  RngSeedManager::SetSeed (rand () % 1000 + 1);
   m_running = true;
   m_packetsSent = 0;
   m_packetsReceived = 0;
-  m_txSocket->Bind (m_sourceAddress);
+  for (uint32_t i = 0; i < m_connections; i++)
+    m_txSocket[i]->Bind (m_sourceAddress[i]);
+  //m_txSocket->ShutdownRecv ();
   m_rxSocket->Bind (m_sinkAddress);
   m_rxSocket->Listen ();
-  m_rxSocket->ShutdownSend ();
+  //m_rxSocket->ShutdownSend ();
   m_rxSocket->SetRecvCallback (MakeCallback (&Worm::HandleRead, this));
   m_rxSocket->SetAcceptCallback (MakeNullCallback<bool, Ptr<Socket>, const Address &> (), MakeCallback (&Worm::HandleAccept, this));
    
@@ -96,9 +101,12 @@ Worm::StopApplication (void)
     Simulator::Cancel (m_sendEvent);
   }
 
-  if (m_txSocket)
+  for (uint32_t i = 0; i < m_connections; i++)
   {
-    m_txSocket->Close ();
+    if (m_txSocket[i])
+    {
+      m_txSocket[i]->Close ();
+    }
   }
     
   while(!m_rxSocketList.empty ())
@@ -117,17 +125,23 @@ Worm::StopApplication (void)
 void 
 Worm::SendPacket (void)
 {
-  m_destinationAddress = InetSocketAddress (GetDestinationIP (), m_port);
-  m_txSocket->Connect (m_destinationAddress);
-  Ptr<Packet> packet = Create<Packet> (m_packetSize);
-  m_txSocket->Send (packet);
-
+  for (uint32_t i = 0; i < m_destinationAddress.size (); i++)
+  {
+    m_destinationAddress.erase (m_destinationAddress.begin () + i);
+  }
+  for (uint32_t i = 0; i < m_connections; i++)
+  {
+    m_destinationAddress.push_back (InetSocketAddress (GetDestinationIP (), m_port));
+    m_txSocket[i]->Connect (m_destinationAddress[i]);
+    Ptr<Packet> packet = Create<Packet> (m_packetSize);
+    m_txSocket[i]->Send (packet);
+    m_packetsSent++;
+  }
   if(m_nPackets == 0)
   {
-    m_packetsSent++;
     ScheduleTx ();
   }
-  else if (m_packetsSent++ < m_nPackets)
+  else if (m_packetsSent < m_nPackets)
   {
     ScheduleTx ();
   }
@@ -186,6 +200,12 @@ Worm::GetTotalRx(void)
 }
 
 void
+Worm::SetType (TypeId typeId)
+{
+  m_typeId = typeId;
+}
+
+void
 Worm::SetFirstIP (uint32_t first)
 {
   firstIP = first;
@@ -231,4 +251,28 @@ Worm::GetDestinationIP (void)
   uint32_t temp = m_listIP[i];
   m_listIP.erase (m_listIP.begin () + i);
   return Ipv4Address (temp);
+}
+
+void
+Worm::SetConnections (uint32_t connections)
+{
+  m_connections = connections;
+}
+
+void
+Worm::SetPacketSize (uint32_t packetSize)
+{
+  m_packetSize = packetSize;
+}
+
+void
+Worm::SetNPackets (uint32_t nPackets)
+{
+  m_nPackets = nPackets;
+}
+
+void
+Worm::SetDataRate (DataRate dataRate)
+{
+  m_dataRate = dataRate;
 }
